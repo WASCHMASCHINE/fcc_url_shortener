df233a14bb5c35bc964e78b431037b866e17246c
Waschmaschine <jonas.haeling@gmail.com>
2016-05-25 10:52:30 +0000

    .env file updated

--- server.js
+++ server.js
@@ -2,15 +2,16 @@
 
 var mongo = require('mongodb').MongoClient;
 var express = require('express');
+var env = require('node-env-file');
+env('.env'); // load .env
 var app = express();
 
 function insertUrlsIntoDatabase(urlObject){
-	mongo.connect("mongodb://localhost:27017/fcc-url-shortener", function(err, db) {
+	mongo.connect(process.env.MONGOLAB_URI, function(err, db) {
 		if (err) throw err;
 		var urls = db.collection('shortenedUrls');
 		urls.insert(urlObject, function(err,data){
 		if (err) throw err;
-		//console.log(JSON.stringify(urlObject));
 		db.close();
 		});
 	});
@@ -19,14 +20,12 @@ function insertUrlsIntoDatabase(urlObject){
 // Create new shortened Url
 app.get('/new/*', function(req, res){
     var originalUrl = req.originalUrl.substr(5); //skip "/new/"
-    
     // Validate URL
     var regexUrl = /https?:\/\/\w+.\w+/;
     if (regexUrl.exec(originalUrl) == null){
     	res.end(JSON.stringify({"error": "URL invalid"}));
     } else {
 		// Check for URL in database? duplicates shouldnt matter, although
-    
 	    // Generate shortened URL
 	    var basisUrl = "https://" + req.headers["host"] + "/";
 	    var shortUrl = basisUrl + Math.random().toString(10).substr(2,6);
@@ -39,13 +38,10 @@ app.get('/new/*', function(req, res){
 
 // Redirect to shortened Url
 app.get('/*', function(req, res){
-    //console.log(url);
-    
     // Look up shortened Url in database
-    //var url = findUrlFromDatabase("https://" + req.headers["host"] + "/" +req.originalUrl.substr(1));
     var url = "https://" + req.headers["host"] + "/" +req.originalUrl.substr(1);
     console.log("hello! ", url);
-   mongo.connect("mongodb://localhost:27017/fcc-url-shortener", function(err, db) {
+   mongo.connect(process.env.MONGOLAB_URI, function(err, db) {
 		if (err) throw err;
 		var urls = db.collection('shortenedUrls');
 		urls.find({short_url: url}).toArray(function(err, items) {
